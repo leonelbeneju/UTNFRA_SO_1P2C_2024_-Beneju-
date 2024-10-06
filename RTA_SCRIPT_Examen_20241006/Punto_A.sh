3713034a84b81bd844f169ec5831af63962c461c
Leonel Beneju <leoo.b18@gmail.com>
2024-10-06 22:17:04 +0000

    add

--- RTA_SCRIPT_Examen_20241006/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -2,7 +2,7 @@
 
 echo 'estructura creada'
 
-mkdir -p $HOME/Examanes-UTN/{alumno{1..3}/parcial{1..3}/profesores} 
+mkdir -p $HOME/Examanes-UTN/{alumno{1..3}/parcial{1..3},profesores} 
 
 
 echo 'mostrar estructura'
