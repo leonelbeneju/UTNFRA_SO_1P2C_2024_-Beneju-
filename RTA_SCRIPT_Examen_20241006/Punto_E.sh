a754ceae610548b97d17b007847487da7cf218e0
Leonel Beneju <leoo.b18@gmail.com>
2024-10-07 02:01:36 +0000

    Add Filtros basico punto E

--- RTA_SCRIPT_Examen_20241006/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo cat /proc/meminfo | grep "MemTotal" >> ~/repogit/UTNFRA_SO_1P2C_2024_-Beneju-/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
+sudo cat /proc/meminfo | grep "MemTotal" >> ~/repogit/UTNFRA_SO_1P2C_2024_-Beneju-/RTA_ARCHIVOS_Examen_20241002/Filtros_Basico.txt
 
-sudo dmidecode -t chassis | awk 'NR>=6 && NR<8' >> ~/repogit/UTNFRA_SO_1P2C_2024_-Beneju-/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
+sudo dmidecode -t chassis | awk 'NR>=6 && NR<8' >> ~/repogit/UTNFRA_SO_1P2C_2024_-Beneju-/RTA_ARCHIVOS_Examen_20241002/Filtros_Basico.txt
 
 
  
